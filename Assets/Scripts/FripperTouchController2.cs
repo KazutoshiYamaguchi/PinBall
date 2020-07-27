7aaa988eb9cf31f6deb30cd042ec17bfe78fc5ed
KazutoshiYamaguchi <umaibou300pon@icloud.com>
2020-07-27 16:45:01 +0900

    PinBall 3DゲームアプリVol.1
    右上に得点を表示しました。ターゲットにボールが衝突したとき得点が加算するようにしました。

---
+++ Assets/Scripts/FripperTouchController2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperTouchController2 : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        double center = Screen.width / 2;
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.touchCount < center && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            //右矢印キーを押した時右フリッパーを動かす
+            if (Input.touchCount > center && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+
+            //矢印キー離された時フリッパーを元に戻す
+            if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score: MonoBehaviour
+{
+    
+    private int score = 0; 
+    private GameObject ScoreText;
+
+
+    void Start()
+    {
+        this.ScoreText = GameObject.Find("ScoreText");
+        score = 0;
+        SetScore();   
+    }
+
+    
+    void OnCollisionEnter(Collision other)
+    {
+        string Tag = other.gameObject.tag;
+
+        if (Tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (Tag == "LargeStarTag") {
+            score += 20;
+        }
+        else if(Tag == "SmallCloudTag")
+        {
+            score += 30;
+        }
+        else if(Tag=="LargeCloudTag")
+        {
+            score += 40;
+        }
+
+
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        ScoreText.GetComponent<Text>().text = string.Format("Score:{0}", score);
+    }
+}
